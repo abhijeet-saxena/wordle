27c79181dbae27fc12e1e35744af4670f369d80a
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2022-01-30 16:43:08 +0530

    Updates

--- main.js
+++ main.js
@@ -1,19 +1,94 @@
-// import db from "./db.js";
+import db from "./db.js";
+const words = Object.keys(db);
+const randomWord = words[Math.floor(Math.random() * words.length)];
+console.log(randomWord);
 
-// const words = Object.keys(db);
+let currentRow = 0;
+let focussedElem = document.querySelector("input");
 
-// const randomWord = words[Math.floor(Math.random() * words.length)];
+document.addEventListener("keyup", (e) => {
+  const { key } = e;
 
-// console.log(randomWord);
+  if (
+    !/[a-z]/i.test(key) ||
+    (key.length > 1 && !["Backspace", "Enter"].includes(key))
+  ) {
+    e.preventDefault();
+    return;
+  }
 
-// console.log("whchw" in db);
+  if (key === "Backspace") {
+    if (
+      !focussedElem.value &&
+      focussedElem.previousElementSibling &&
+      !focussedElem.previousElementSibling.disabled
+    ) {
+      focussedElem = focussedElem.previousElementSibling;
+    }
 
-document.querySelector(".grid").addEventListener("input", (e) => {
-  const {
-    data,
-    target,
-    target: { tagName },
-  } = e;
-  console.log({ data, target, tagName });
-  e.target.nextElementSibling.focus();
+    focussedElem.value = "";
+    focussedElem.classList.remove("filled");
+  } else if (key === "Enter") {
+    const inputs = Array.from(document.querySelectorAll("input")).slice(
+      currentRow,
+      currentRow + 5
+    );
+    const guessedWord = inputs.map((i) => i.value).join("");
+
+    if (!(guessedWord in db)) {
+      alert("Wrong Word", guessedWord);
+      return;
+    }
+
+    for (let i in inputs) {
+      setTimeout(setCorrectClass, i * 250, inputs[i], i);
+    }
+
+    setTimeout(() => {
+      if (randomWord.toLowerCase() === guessedWord.toLowerCase()) {
+        alert("You W0n");
+      } else {
+        currentRow += 5;
+        disableInputs(currentRow);
+      }
+    }, 1500);
+  } else {
+    focussedElem.classList.add("filled");
+    focussedElem.value = focussedElem.value || e.key;
+    if (!focussedElem.nextElementSibling?.disabled) {
+      focussedElem = focussedElem.nextElementSibling;
+    }
+  }
+});
+
+function disableInputs(enabled) {
+  const inputs = document.querySelectorAll("input");
+
+  inputs.forEach((i, index) => {
+    i.disabled = index < enabled || index > enabled + 4 ? true : false;
+  });
+
+  focussedElem = document.querySelectorAll("input")[enabled];
+}
+
+function setCorrectClass(elem, i) {
+  if (randomWord[i].toLowerCase() === elem.value.toLowerCase()) {
+    elem.classList.add("correct");
+  } else if (randomWord.includes(elem.value.toLowerCase())) {
+    elem.classList.add("maybe");
+  } else {
+    elem.classList.add("wrong");
+  }
+}
+
+disableInputs(currentRow);
+
+document.querySelector("#keyboard").addEventListener("click", (e) => {
+  if (e.target.tagName !== "BUTTON") return;
+
+  document.dispatchEvent(
+    new KeyboardEvent("keyup", {
+      key: e.target.dataset.val,
+    })
+  );
 });
